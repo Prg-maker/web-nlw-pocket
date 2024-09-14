60b5a2dbb2e9fdc022ab51eb5e084f2a2687435a
prg-maker <df3296709@gmail.com>
2024-09-14 10:34:37 -0300

    add bar progresion

---
+++ src/components/summary.tsx
@@ -0,0 +1,25 @@
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import { InOrbitIcon } from './ui/in-orbit-icon'
+export function Summary() {
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6 ">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de Agosto</span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3"></div>
+    </div>
+  )
+}
